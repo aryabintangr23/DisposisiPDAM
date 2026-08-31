50e75c43a3caeaa60090f4676bb193d0bed80b13
aryabintangr23 <arya74578@gmail.com>
2026-08-31 15:20:43 +0700

    Memperbarui web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DisposisiController;
+use App\Http\Controllers\SuratController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', [LoginController::class, 'create'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'store'])->middleware('guest');
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout')->middleware('auth');
+
+Route::middleware('auth')->group(function () {
+    Route::redirect('/', '/surat');
+
+    Route::get('/surat', [SuratController::class, 'index'])->name('surat.index');
+    Route::get('/surat/create', [SuratController::class, 'create'])->name('surat.create');
+    Route::post('/surat', [SuratController::class, 'store'])->name('surat.store');
+    Route::get('/surat/{surat}', [SuratController::class, 'show'])->name('surat.show');
+
+    Route::post('/surat/{surat}/disposisi', [DisposisiController::class, 'store'])->name('disposisi.store');
+    Route::post('/surat/{surat}/disposisi/{disposisi}/selesai', [DisposisiController::class, 'selesaikan'])->name('disposisi.selesaikan');
 });
