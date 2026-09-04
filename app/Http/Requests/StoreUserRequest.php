3c737d48944a58c83ea89bb78ef65c66adf9dc5f
Syahdan <belajarmandiri484@gmail.com>
2026-09-04 09:38:34 +0700

    admin

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Password;
 
+
 class StoreUserRequest extends FormRequest
 {
     public function authorize(): bool
